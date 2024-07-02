bb32b72a9c2d1301cf005d408b9eb907e13de517
Rohit Anand Vanita shinde <50909065+shinderohit@users.noreply.github.com>
2024-07-02 05:21:45 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14485
+let accountEmail = "rohit@google.com"
+var accountPassword = "12345"  
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = 23 not allowed
+accountEmail ="rs@google.com"
+accountPassword = "21344"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+let name = "rohit" //string
+let age = 25 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to power 53
+// bigInt
+// string = use ""
+// boolean = true or false
+// null = stand alone value 
+// undefined = 
+// symbol = define unique
+
+
+// object 
+
+console.log(typeof "rohit")
+console.log(typeof null)
