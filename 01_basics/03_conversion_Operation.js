25b7e3c8884eb77ba757f46e3512cf52d40733af
Rohit Anand Vanita shinde <50909065+shinderohit@users.noreply.github.com>
2024-07-02 05:46:13 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,24 @@
+let score = "rohit"
+// console.log (typeof score);
+
+let  valueInNumber = Number(score)
+// console.log (typeof valueInNumber);
+// console.log (valueInNumber);
+
+// "33" = 33
+// "33abs" = NaN
+// true = 1; false = 0
+
+let isLoggedIn = "rohit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// true = 1; false = 0
+// " " = false
+// "Rohit" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
