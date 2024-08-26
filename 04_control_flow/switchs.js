c43a5bc4482593b08dc8f7a107a5125e9cc4cfd0
shinderohit <shinderohit688@gmail.com>
2024-08-26 13:00:09 +0530

    Control_Switch

---
+++ 04_control_flow/switchs.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 1:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default");
+//         break;
+// }
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default");
+        break;
+}
