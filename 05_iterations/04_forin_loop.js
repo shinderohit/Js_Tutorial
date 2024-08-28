9dd5e71fa837eca9f245960c4a2d34deb5f1cc5a
shinderohit <shinderohit688@gmail.com>
2024-08-28 12:40:07 +0530

    For In Loop

---
+++ 05_iterations/04_forin_loop.js
@@ -0,0 +1,35 @@
+const myObject = {
+    js : "javascript",
+    cpp : "C++",
+    rb : "ruby",
+    rn : "React Native"
+}
+
+// for (const key in myObject) {
+//     console.log(key);
+// }
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} : ${myObject[key]}`);
+// }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+
+// const map = new Map()
+// map.set('IND', "India")
+// map.set('IND', "India")
+// map.set('USA', "United State of America")
+// map.set('FRA', "France")
+
+// for (const key in map) {
+//     console.log(key);
+// }
