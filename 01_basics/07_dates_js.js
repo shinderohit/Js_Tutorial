46f1a331cc62c8d6d87f0e5ca9c8c8a92a0f18ff
shinderohit <shinderohit688@gmail.com>
2024-10-08 11:27:46 +0530

    Date & Time

--- 01_basics/07_dates_js.js
+++ 01_basics/07_dates_js.js
@@ -8,22 +8,20 @@ let myDate = new Date()
 // console.log(myDate.toLocaleTimeString());
 // console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
+let myCreatedDate = new Date(2023, 0, 23)
 // console.log(myCreatedDate.toDateString());
-
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// console.log(myCreatedDate.toLocaleString());
-
-
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2024")
-// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate1 = new Date(2023, 11, 23, 5, 3)
+// console.log(myCreatedDate1.toLocaleString());
+let myCreatedDate2 = new Date("2023-01-14")
+// console.log(myCreatedDate2.toLocaleString());
+let myCreatedDate3 = new Date("01-14-2024")
+// console.log(myCreatedDate3.toLocaleString());
 
 let myTimeStamp = Date.now()
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Date.now()/1000);
-// console.log(Math.floor(Date.now()/1000));
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Date.now()/1000);
+console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
 // console.log(newDate);
@@ -31,7 +29,7 @@ let newDate = new Date()
 // console.log(newDate.getMonth()+1);
 // console.log(newDate.getDay());
 
-newDate.toLocaleString('default',{
-    weekday:"long",
-    timeZone:''
-})
+// newDate.toLocaleString('default',{
+//     weekday:"long",
+//     timeZone:''
+// })
