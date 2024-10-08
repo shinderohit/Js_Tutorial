4f5eb61a97aeb19aad425d9120e4a81949399650
shinderohit <shinderohit688@gmail.com>
2024-10-08 11:35:34 +0530

    Date & Time

--- 01_basics/07_dates_js.js
+++ 01_basics/07_dates_js.js
@@ -18,16 +18,16 @@ let myCreatedDate3 = new Date("01-14-2024")
 // console.log(myCreatedDate3.toLocaleString());
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
-console.log(Date.now()/1000);
-console.log(Math.floor(Date.now()/1000));
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
 // console.log(newDate);
 // console.log(newDate.getMonth());
 // console.log(newDate.getMonth()+1);
-// console.log(newDate.getDay());
+// console.log(newDate.ge tDay());
 
 // newDate.toLocaleString('default',{
 //     weekday:"long",
